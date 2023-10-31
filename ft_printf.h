b68f841f1b58ee22073b0460be04ca90e2105625
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2023-10-31 16:34:29 +0100

    0.2

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,23 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 16:59:38 by ade-beco          #+#    #+#             */
-/*   Updated: 2023/10/26 16:59:40 by ade-beco         ###   ########.fr       */
+/*   Updated: 2023/10/31 16:04:28 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+#include <unistd.h>
+
+# include "libft/libft.h"
+
+// common
+void	ft_putchar(char a);
+
+
+// utils
+void	ft_putchar(char a);
+
+
+#endif
--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -1,32 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/26 16:59:07 by ade-beco          #+#    #+#             */
-/*   Updated: 2023/10/31 16:26:19 by ade-beco         ###   ########.fr       */
+/*   Created: 2023/10/31 16:00:57 by ade-beco          #+#    #+#             */
+/*   Updated: 2023/10/31 16:04:42 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *str, ...)
+void	ft_putchar(char a)
 {
-	unsigned int	i;
-
-	i = 0;
-
-	while (str[i] != '\0')
-	{
-		if (ft_isprint(str[i]))
-			ft_putchar(str[i]);
-	}
-	return (1);
+	write(1, &a, 1);
 }
-
-/*int	main()
-{
-	ft_printf("gfshghfdg4564");
-}*/
